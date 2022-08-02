bb2f995fca402e79fa8843d53db99289af4a0b86
AyubuAyubu <ayubu6143@gmail.com>
2022-08-02 20:42:05 +0300

    Done

---
+++ index.js
@@ -0,0 +1,59 @@
+function add(m, n){
+    return m + n;
+}
+
+function subtract(m, n){
+    return m - n;
+}
+
+function multiply(m, n){
+    return m* n;
+}
+
+function divide(m, n){
+    return m/n;
+}
+
+function increment(m){
+    return ++n;
+}
+
+function decrement(m){
+    return --n;
+}
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string, 10);
+}
+
+console.log(add(6,2));
+console.log(subtract(8,4));
+console.log(multiply(4,10));
+console.log(divide(9,9));
+
+console.log(increment(8));
+console.log(decrement(8));
+console.log(makeInt('89'));
+console.log(makeInt('Msa'));
+console.log(preserveDecimal('78.777'));
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string, 10);
+}
+
+console.log(add(2,2));
+console.log(subtract(2,1));
+console.log(multiply(42,10));
+console.log(divide(9,3));
+
+console.log(increment(7));
+console.log(decrement(7));
+console.log(makeInt('69'));
+console.log(makeInt('Candy'));
+console.log(preserveDecimal('7.777'));
